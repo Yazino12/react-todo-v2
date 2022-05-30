9eb815413d198732d51fa787ddf63780c04f5476
yasin <yasin.elsharawi23@gmail.com>
2022-05-30 21:07:06 +0300

    Add form and handle events Add form for handling user input Add checkboxes to the Todo items Raise and handle events Delete items from the todos functionality Generate random ids using uuid Validate from submission

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,26 +1,61 @@
 import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import Header from './Header';
 import TodoList from './TodoList';
+import InputTodo from './InputTodo';
 
 class TodoContainer extends React.Component {
   state = {
     todos: [
       {
-        id: 1,
+        id: uuidv4(),
         title: 'Setup development environment',
         completed: true,
       },
       {
-        id: 2,
+        id: uuidv4(),
         title: 'Develop website and add content',
         completed: false,
       },
       {
-        id: 3,
+        id: uuidv4(),
         title: 'Deploy to live server',
         completed: false,
       },
     ],
+  };
+
+  handleChange = (id) => {
+    this.setState((prevState) => ({
+      todos: prevState.todos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        }
+        return todo;
+      }),
+    }));
+  };
+
+  addTodoItem = (title) => {
+    const newTodo = {
+      id: uuidv4(),
+      title,
+      completed: false,
+    };
+    this.setState({
+      todos: [...this.state.todos, newTodo],
+    });
+  };
+
+  delTodo = (id) => {
+    this.setState({
+      todos: [
+        ...this.state.todos.filter((todo) => todo.id !== id),
+      ],
+    });
   };
 
   render() {
@@ -28,6 +63,12 @@ class TodoContainer extends React.Component {
       <div>
         <Header />
         <TodoList todos={this.state.todos} />
+        <InputTodo addTodoProps={this.addTodoItem} />
+        <TodoList
+          todos={this.state.todos}
+          handleChangeProps={this.handleChange}
+          deleteTodoProps={this.delTodo}
+        />
       </div>
     );
   }
